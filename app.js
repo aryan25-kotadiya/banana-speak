c6b4ee13f73ce1d87abb9a45a80dbfc7cf7bd842
aryan25-kotadiya <aryankotadiya25@Gmail.com>
2025-03-03 19:14:35 +0530

    final js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
+
+
 function getTranslationURL(text) {
-    return serverURL + "?text=" + encodeURIComponent(text);
+    return serverURL + "?" + "text="  + text
 }
 
 function errorHandler(error) {
@@ -13,15 +16,18 @@ function errorHandler(error) {
     alert("Something went wrong with the server! Please try again later.");
 }
 
+
 function clickHandler() {
     var inputText = txtInput.value;
+
 
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
-            outputDiv.textContent = json.contents.translated;
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
         })
         .catch(errorHandler);
-}
+};
 
 btnTranslate.addEventListener("click", clickHandler);
